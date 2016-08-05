a7f5ab1eb7ca11dffbf8e3eb3b203626eb1f6208
Kay Lovelace <kay@herostrat.us>
2016-08-05 19:29:00 +0100

    Refine pattern matching in PathResolver

--- src/main/scala/troglodyte/opendatr/resolvers/PathResolver.scala
+++ src/main/scala/troglodyte/opendatr/resolvers/PathResolver.scala
@@ -3,13 +3,11 @@ package troglodyte.opendatr.resolvers
 import java.io.File
 
 class PathResolver extends Resolver {
-  override def canResolve(puzzle: Any): Boolean = {
-    puzzle match {
-      case path: String =>
-        new File(path).exists()
-      case _ =>
-        false
-    }
+  override def canResolve(puzzle: Any): Boolean = puzzle match {
+    case path: String =>
+      new File(path).exists()
+    case _ =>
+      false
   }
 
   override def resolve(puzzle: Any): Any = {
