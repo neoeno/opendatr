c777b2b6be282147df71cf0741bc2acaf05effd9
Kay Lovelace <kay@herostrat.us>
2016-08-05 19:10:23 +0100

    Test QueenResolver using other resolvers

--- src/test/scala/troglodyte/opendatr/resolvers/QueenResolverTest.scala
+++ src/test/scala/troglodyte/opendatr/resolvers/QueenResolverTest.scala
@@ -1,6 +1,6 @@
 package troglodyte.opendatr.resolvers
 
-import java.io.PrintStream
+import java.io.{File, PrintStream}
 
 import org.scalatest.FunSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
@@ -11,9 +11,9 @@ class QueenResolverTest extends FunSpec with GeneratorDrivenPropertyChecks {
     describe("#canResolve") {
       it("can't actually resolve anything yet") {
         val announcerPair = TestFactory.makeAnnouncer()
-        val queenResolver = new QueenResolver(announcerPair._1, List())
+        val resolver = new QueenResolver(announcerPair._1, List())
         forAll { (anything: String) =>
-          assert(queenResolver.canResolve(anything) === false)
+          assert(resolver.canResolve(anything) === false)
         }
       }
     }
@@ -22,9 +22,48 @@ class QueenResolverTest extends FunSpec with GeneratorDrivenPropertyChecks {
       it("reports its disappointment") {
         forAll { (anything: String) =>
           val announcerPair = TestFactory.makeAnnouncer()
-          val queenResolver = new QueenResolver(announcerPair._1, List())
-          queenResolver.resolve(anything)
+          val resolver = new QueenResolver(announcerPair._1, List())
+          resolver.resolve(anything)
           assert(announcerPair._2.toString.contains("Sorry, couldn't resolve this one! You're on your own!"))
+        }
+      }
+    }
+  }
+
+  describe("with a singleton list of PathResolver") {
+    val announcerPair = TestFactory.makeAnnouncer()
+    val resolver = new QueenResolver(announcerPair._1, List(new PathResolver))
+
+    describe("and a valid path") {
+      val tempFile = File.createTempFile("temp", "file")
+
+      describe("#canResolve") {
+        it("is true") {
+          assert(resolver.canResolve(tempFile.getAbsolutePath))
+        }
+      }
+
+      describe("#resolve") {
+        it("resolves to a File") {
+          val resolved = resolver.resolve(tempFile.getAbsolutePath)
+          assert(resolved.isInstanceOf[File])
+          assert(resolved.asInstanceOf[File].getAbsolutePath == tempFile.getAbsolutePath)
+        }
+      }
+    }
+
+    describe("and an invalid path") {
+      describe("#canResolve") {
+        it("is false") {
+          assert(resolver.canResolve("non/extant/path") === false)
+        }
+      }
+    }
+
+    describe("and a random object") {
+      describe("#canResolve") {
+        it("is false") {
+          assert(resolver.canResolve(new Object) === false)
         }
       }
     }
