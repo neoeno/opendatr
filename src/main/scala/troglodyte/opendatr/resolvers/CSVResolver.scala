984035ccc7134729868244fa19de0e5f67c4406e
Kay Lovelace <kay@herostrat.us>
2016-08-07 16:31:51 +0100

    Neaten up CSVResolver

--- src/main/scala/troglodyte/opendatr/resolvers/CSVResolver.scala
+++ src/main/scala/troglodyte/opendatr/resolvers/CSVResolver.scala
@@ -1,6 +1,6 @@
 package troglodyte.opendatr.resolvers
 
-import java.io.File
+import java.io.{Closeable, File}
 
 import com.github.tototoshi.csv.CSVReader
 import troglodyte.opendatr.askers.Asker
@@ -17,30 +17,35 @@ class CSVResolver(asker: Asker) extends Resolver {
     case _ => false
   }
 
-  def hasHeadings(file: File): Boolean = {
+  override def resolve(puzzle: Any): Option[Any] = {
+    val file = puzzle.asInstanceOf[File]
+    withClosing(CSVReader.open(file)) { (reader: CSVReader) =>
+      Some(new Dataset(
+        if (hasHeadings(file)) {
+          reader.iteratorWithHeaders.map { (row: Map[String, String]) =>
+            new Entity(row)
+          }.toList
+        } else {
+          reader.iterator.map { (row: Seq[String]) =>
+            // should these keys really be strings?
+            new Entity(
+              row.zipWithIndex.map(pair => (pair._2.toString, pair._1)).toMap
+            )
+          }.toList
+        }
+      ))
+    }
+  }
+
+  private def hasHeadings(file: File): Boolean = {
     val headingReader = CSVReader.open(file)
     asker.show(headingReader.iterator.take(1).toList.head.mkString(", "))
     headingReader.close()
     asker.askYesNo('has_headings, s"Do these look like column headings to you? [Yn] ")
   }
 
-  override def resolve(puzzle: Any): Option[Any] = {
-    val file = puzzle.asInstanceOf[File]
-    val reader = CSVReader.open(file)
-    val retVal = Some(new Dataset(
-      if (hasHeadings(file)) {
-        reader.iteratorWithHeaders.map { (row: Map[String, String]) =>
-          new Entity(row)
-        }.toList
-      } else {
-        reader.iterator.map { (row: Seq[String]) =>
-          // should these keys really be strings?
-          new Entity(
-            row.zipWithIndex.map(pair => (pair._2.toString, pair._1)).toMap
-          )
-        }.toList
-      }
-    ))
+  private def withClosing[T, R <: Closeable](reader: R)(f: R => T): T = {
+    val retVal = f(reader)
     reader.close()
     retVal
   }
